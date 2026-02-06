508d90fd3e36a6e703648b8b973b040aeea686ee
Syed Saad Qadeer <saadsyed454@gmail.com>
2026-02-06 11:07:53 -0500

    BigQuery ingesation

--- ingest_raw_script.py
+++ ingest_raw_script.py
@@ -1,28 +1,39 @@
 import pandas as pd
+from google.cloud import bigquery
 import random
-from datetime import datetime, timedelta
+from datetime import datetime
 
-def generate_daily_batch(date):
-    """Simulates raw API data for a specific date."""
-    user_ids = range(100, 150)
-    event_types = ['signup', 'upgrade', 'downgrade', 'churn']
-    plans = ['Basic', 'Pro', 'Enterprise']
-    
+def get_stripe_data():
+    """Simulates raw Stripe API response and flattens it."""
     data = []
-    for _ in range(20):  # 20 events per day
+    for _ in range(10):
         data.append({
-            'event_id': random.randint(1000, 9999),
-            'customer_id': random.choice(user_ids),
-            'event_type': random.choice(event_types),
-            'plan_name': random.choice(plans),
-            'amount': random.choice([10, 50, 200]),
-            'event_timestamp': date.strftime('%Y-%m-%d %H:%M:%S')
+            'sub_id': f"sub_{random.randint(1000, 9999)}",
+            'customer_id': f"cus_{random.randint(100, 500)}",
+            'status': random.choice(['active', 'canceled', 'past_due']),
+            'plan_name': random.choice(['Basic', 'Pro', 'Enterprise']),
+            'monthly_amount_cents': random.choice([1000, 5000, 20000]),
+            'period_start_ts': datetime.now(),
+            'ingested_at': datetime.now()
         })
     return pd.DataFrame(data)
 
-# Execution
-today = datetime.now()
-df = generate_daily_batch(today)
-filename = f"raw_sub_events_{today.strftime('%Y%m%d')}.csv"
-df.to_csv(f"ingestion/{filename}", index=False)
-print(f"✅ Bronze Layer: {filename} created successfully.")
+def upload_to_bigquery(df):
+    # This automatically uses the GCP_SA_KEY secret from GitHub Actions
+    client = bigquery.Client()
+    
+    # Update this with your actual Project ID from Google Cloud
+    table_id = "ltv-sentinel-project.ltv_sentinel_bronze.stripe_raw_subs"
+
+    job_config = bigquery.LoadJobConfig(
+        write_disposition="WRITE_APPEND", # This keeps historical data
+    )
+
+    print(f"🚀 Sentinel: Uploading {len(df)} rows to BigQuery...")
+    job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
+    job.result() 
+    print("✅ Success: Data is now in the Bronze Layer.")
+
+if __name__ == "__main__":
+    df = get_stripe_data()
+    upload_to_bigquery(df)
